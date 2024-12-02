888af691fe8515b535713a6eb34ad2de06d23633
Alexis Costedoat <67351701+costeva@users.noreply.github.com>
2024-12-02 13:43:06 -0300

    Armado de componete list y vista con acciones de filtrado.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <button @click="handleClick('title')">Order by title</button>
+    <button @click="handleClick('location')">Order by location</button>
+    <button @click="handleClick('salary')">Order by salary</button>
+    <list-jobs :jobs="jobs" :order="order" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import Job from "./types/jobs";
+import listJobs from "./components/jobsList.vue";
+import OrderTerm from "./types/orderTerm";
+export default defineComponent({
+  name: "welcome",
+  components: {
+    listJobs,
+  },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        id: 1,
+        title: "Frontend Developer",
+        location: "Madrid",
+        salary: 30000,
+      },
+      {
+        id: 2,
+        title: "Backend Developer",
+        location: "Barcelona",
+        salary: 40000,
+      },
+      {
+        id: 3,
+        title: "Fullstack Developer",
+        location: "Valencia",
+        salary: 50000,
+      },
+    ]);
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return {
+      order,
+      jobs,
+      handleClick,
+    };
+  },
+});
+</script>
---
+++ src/components/jobsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>Jobs List</h2>
+    <p>Order by: {{ order }}</p>
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h3>{{ job.title }} in {{ job.location }}</h3>
+        <div class="salary">Salary: {{ job.salary }}</div>
+        <div class="desciption">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
+            repellendus obcaecati a iusto. Culpa error, ipsam dolores dicta
+            placeat obcaecati! Ad est quis enim officia similique culpa odio
+            magni quos!
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, Prop, PropType } from "vue";
+import OrderTerm from "../types/orderTerm";
+import Job from "./types/jobs";
+
+export default defineComponent({
+  name: "component-jobs-list",
+  props: {
+    jobs: {
+      type: Array as PropType<Job[]>,
+      required: true,
+    },
+    order: {
+      type: String as PropType<OrderTerm>,
+      required: true,
+    },
+  },
+  setup() {},
+});
+</script>
